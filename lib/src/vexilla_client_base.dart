01ca80dabd127e50d97ccab44ba51e5b82d466b3
Morten <mortenutiger@protonmail.comgit config --global user.email mortenutiger@protonmail.comgit config --global user.email mortenutiger@protonmail.com>
2021-04-10 11:23:19 +0200

    migrated to null safety

--- lib/src/vexilla_client_base.dart
+++ lib/src/vexilla_client_base.dart
@@ -8,21 +8,21 @@ const vexillaFeatureTypeGradual = 'gradual';
 class VexillaClient {
   final String _baseUrl, _environment, _customInstanceHash;
 
-  Map _flags;
+  Map? _flags;
 
   VexillaClient(this._environment, this._baseUrl, this._customInstanceHash);
 
-  Future<Map> fetchFlags(String fileName) async {
+  Future<Map?> fetchFlags(String fileName) async {
     var response = await http.get(Uri.parse('$_baseUrl/$fileName'));
     return jsonDecode(response.body);
   }
 
-  void setFlags(Map flags) {
+  void setFlags(Map? flags) {
     _flags = flags;
   }
 
-  bool should(String featureName) {
-    var feature = _flags['environments'][_environment]['untagged'][featureName];
+  bool? should(String featureName) {
+    var feature = _flags!['environments'][_environment]['untagged'][featureName];
 
     if (feature['type'] == vexillaFeatureTypeToggle) {
       return feature['value'];
